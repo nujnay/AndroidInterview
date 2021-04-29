9b4bd34a64cacb464a33cc16d3f22a744e538da1
yanjun <1292015395@qq.com>
2021-04-29 15:28:50 +0800

    master

--- 03_Algorithm/Microsoft/Test_04.java
+++ 03_Algorithm/Microsoft/Test_04.java
@@ -11,7 +11,10 @@ package Microsoft;
  * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
  */
 public class Test_04 {
-
+    public static void main(String[] args) {
+        Solution04 solution04 = new Solution04();
+        System.out.println(solution04.reverseWords("A Apple"));
+    }
 }
 
 class Solution04 {
