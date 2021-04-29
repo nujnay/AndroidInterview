d5a85a47ce4bbb9ee6fbe558850c9d43102a2b1d
yanjun <1292015395@qq.com>
2021-04-29 11:11:58 +0800

    master

--- 03_Algorithm/Microsoft/Test_04.java
+++ 03_Algorithm/Microsoft/Test_04.java
@@ -3,7 +3,12 @@ package Microsoft;
 /**
  * Write a function that reverses a string. The input string is given as an array of characters s.
  *
+ * Follow up: Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
  *
+ * 作者：力扣 (LeetCode)
+ * 链接：https://leetcode-cn.com/leetbook/read/microsoft/ba8uv/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
  *
  */
 public class Test_04 {
