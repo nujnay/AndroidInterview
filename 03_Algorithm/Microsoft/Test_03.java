b5d6bc8f19eb1f684449e2219b407981646e59fc
yanjun <1292015395@qq.com>
2021-04-29 10:27:05 +0800

    master

--- 03_Algorithm/Microsoft/Test_03.java
+++ 03_Algorithm/Microsoft/Test_03.java
@@ -1,10 +1,12 @@
 package Microsoft;
 
+import java.util.Arrays;
+
 /**
  * Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
- *
+ * <p>
  * The algorithm for myAtoi(string s) is as follows:
- *
+ * <p>
  * Read in and ignore any leading whitespace.
  * Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
  * Read in next the characters until the next non-digit charcter or the end of the input is reached. The rest of the string is ignored.
@@ -12,16 +14,15 @@ package Microsoft;
  * If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
  * Return the integer as the final result.
  * Note:
- *
+ * <p>
  * Only the space character ' ' is considered a whitespace character.
  * Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
  *  
- *
+ * <p>
  * 作者：力扣 (LeetCode)
  * 链接：https://leetcode-cn.com/leetbook/read/microsoft/bm6tg/
  * 来源：力扣（LeetCode）
  * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
- *
  */
 public class Test_03 {
     public static void main(String[] args) {
@@ -29,105 +30,49 @@ public class Test_03 {
     }
 
 
-
 }
 class Solution {
-    public int myAtoi(String s) {
-        char[] chars = s.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            if (i == 0) {
-                if (isAlphanumeric(chars[i])) {
-                    return 0;
-                }
-            }
+    public int myAtoi(String str) {
+        Automaton automaton = new Automaton();
+        int length = str.length();
+        for (int i = 0; i < length; ++i) {
+            automaton.get(str.charAt(i));
         }
-        try {
-            Integer integer = Integer.valueOf(after);
-            return integer;
-        } catch (Exception e) {
-            return 0;
+        return (int) (automaton.sign * automaton.ans);
+    }
+}
+
+class Automaton {
+    public int sign = 1;
+    public long ans = 0;
+    private String state = "start";
+    private Map<String, String[]> table = new HashMap<String, String[]>() {{
+        put("start", new String[]{"start", "signed", "in_number", "end"});
+        put("signed", new String[]{"end", "end", "in_number", "end"});
+        put("in_number", new String[]{"end", "end", "in_number", "end"});
+        put("end", new String[]{"end", "end", "end", "end"});
+    }};
+
+    public void get(char c) {
+        state = table.get(state)[get_col(c)];
+        if ("in_number".equals(state)) {
+            ans = ans * 10 + c - '0';
+            ans = sign == 1 ? Math.min(ans, (long) Integer.MAX_VALUE) : Math.min(ans, -(long) Integer.MIN_VALUE);
+        } else if ("signed".equals(state)) {
+            sign = c == '+' ? 1 : -1;
         }
     }
 
-    private boolean isAlphanumeric(char c) {
-        if (c == 'a') {
-            return true;
+    private int get_col(char c) {
+        if (c == ' ') {
+            return 0;
         }
-        if (c == 'b') {
-            return true;
+        if (c == '+' || c == '-') {
+            return 1;
         }
-        if (c == 'c') {
-            return true;
+        if (Character.isDigit(c)) {
+            return 2;
         }
-        if (c == 'd') {
-            return true;
-        }
-        if (c == 'e') {
-            return true;
-        }
-        if (c == 'f') {
-            return true;
-        }
-        if (c == 'g') {
-            return true;
-        }
-        if (c == 'h') {
-            return true;
-        }
-        if (c == 'i') {
-            return true;
-        }
-        if (c == 'j') {
-            return true;
-        }
-        if (c == 'k') {
-            return true;
-        }
-        if (c == 'l') {
-            return true;
-        }
-        if (c == 'm') {
-            return true;
-        }
-        if (c == 'n') {
-            return true;
-        }
-        if (c == 'o') {
-            return true;
-        }
-        if (c == 'p') {
-            return true;
-        }
-        if (c == 'q') {
-            return true;
-        }
-        if (c == 'r') {
-            return true;
-        }
-        if (c == 's') {
-            return true;
-        }
-        if (c == 't') {
-            return true;
-        }
-        if (c == 'u') {
-            return true;
-        }
-        if (c == 'v') {
-            return true;
-        }
-        if (c == 'w') {
-            return true;
-        }
-        if (c == 'x') {
-            return true;
-        }
-        if (c == 'y') {
-            return true;
-        }
-        if (c == 'z') {
-            return true;
-        }
-        return false;
+        return 3;
     }
 }
