48564fde3a22c864707e4dad2ff4faf6e95d29e2
yanjun <1292015395@qq.com>
2021-05-04 08:49:31 +0800

    master

--- 03_Algorithm/Microsoft/chapter_a/Test_09.java
+++ 03_Algorithm/Microsoft/chapter_a/Test_09.java
@@ -1,4 +1,13 @@
 package Microsoft.chapter_a;
 
+import java.util.List;
+
 public class Test_09 {
+
+}
+
+class Solution_a_09 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+    }
 }
