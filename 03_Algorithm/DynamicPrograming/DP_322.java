c7e59dec03489bea18dda6b9f203b4776b3b6faa
yanjun <1292015395@qq.com>
2021-05-04 08:47:54 +0800

    master

--- 03_Algorithm/DynamicPrograming/DP_322.java
+++ 03_Algorithm/DynamicPrograming/DP_322.java
@@ -8,17 +8,17 @@ public class DP_322 {
     }
 }
 
-class Solution_322 {
-    public int coinChange(int[] coins, int amount) {
-        int max = amount + 1;
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, max);
-        dp[0] = 0;
-        for (i = 1; i <= amount; ++i) {
-            f[i] = Integer.MAX_VALUE;
-            for (j = 0; j < n; j++) {
-                f[i] = Math.min(f[i - coins[j]] + 1, f[i]);
-            }
-        }
-    }
-}
+//class Solution_322 {
+//    public int coinChange(int[] coins, int amount) {
+//        int max = amount + 1;
+//        int[] dp = new int[amount + 1];
+//        Arrays.fill(dp, max);
+//        dp[0] = 0;
+//        for (i = 1; i <= amount; ++i) {
+//            f[i] = Integer.MAX_VALUE;
+//            for (j = 0; j < n; j++) {
+//                f[i] = Math.min(f[i - coins[j]] + 1, f[i]);
+//            }
+//        }
+//    }
+//}
