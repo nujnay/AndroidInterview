1a4cbab6822de84385167bb90f665f547c63e949
yanjun <1292015395@qq.com>
2021-05-04 08:59:41 +0800

    master

--- 03_Algorithm/Microsoft/chapter_a/Test_09.java
+++ 03_Algorithm/Microsoft/chapter_a/Test_09.java
@@ -1,5 +1,8 @@
 package Microsoft.chapter_a;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Test_09 {
@@ -8,6 +11,19 @@ public class Test_09 {
 
 class Solution_a_09 {
     public List<List<String>> groupAnagrams(String[] strs) {
-
+        HashMap<Character[], List<String>> map = new HashMap<>();
+        List<List<String>> list = new ArrayList<>();
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            if (map.containsKey(chars)) {
+                map.get(chars).add(s);
+            } else {
+                ArrayList<String> arrayList = new ArrayList<>();
+                arrayList.add(s);
+                list.add(arrayList);
+            }
+        }
+        return list;
     }
 }
