5f1cc7aaccf2846bd6fc81fc6c3f7bd51d83017c
yanjun <1292015395@qq.com>
2021-05-04 17:33:07 +0800

    master

--- 03_Algorithm/Microsoft/chapter_a/Test_09.java
+++ 03_Algorithm/Microsoft/chapter_a/Test_09.java
@@ -6,23 +6,33 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Test_09 {
-
+    public static void main(String[] args) {
+        Solution_a_09 solution_a_09 = new Solution_a_09();
+        String[] strings = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        solution_a_09.groupAnagrams(strings);
+    }
 }
 
 class Solution_a_09 {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<Character[], List<String>> map = new HashMap<>();
-        List<List<String>> list = new ArrayList<>();
+        HashMap<String, List<String>> map = new HashMap<>();
         for (String s : strs) {
             char[] chars = s.toCharArray();
             Arrays.sort(chars);
-            if (map.containsKey(chars)) {
-                map.get(chars).add(s);
+            String s1 = new String(chars);
+            if (map.containsKey(s1)) {
+                map.get(s1).add(s);
             } else {
                 ArrayList<String> arrayList = new ArrayList<>();
                 arrayList.add(s);
-                list.add(arrayList);
+                map.put(s1, arrayList);
             }
+        }
+        List<List<String>> list = new ArrayList<>();
+        for (String chars : map.keySet()) {
+            System.out.println(chars);
+            List<String> list1 = map.get(chars);
+            list.add(list1);
         }
         return list;
     }
