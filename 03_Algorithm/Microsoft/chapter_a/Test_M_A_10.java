73b46e2c8b2afc3d159a6aea78d94dde57b2f41d
yanjun <1292015395@qq.com>
2021-05-05 17:20:45 +0800

    master

--- 03_Algorithm/Microsoft/chapter_a/Test_M_A_10.java
+++ 03_Algorithm/Microsoft/chapter_a/Test_M_A_10.java
@@ -1,36 +1,36 @@
-package Microsoft.chapter_a;
-
-public class Test_M_A_10 {
-
-}
-class Solution_M_A_10 {
-    //f
-    public int trap(int[] height) {
-        int backIndex;
-        int backHeight = 0;
-        int frontIndex;
-        int frontHeight = 0;
-        int brick;
-        int nowWater = 0;
-        for (int i = 0; i < height.length; i++){
-            int nowHeight = height[i];
-            if (nowHeight > 0) {
-                if(frontHeight == 0) {
-                    frontIndex = i;
-                    frontHeight = nowHeight;
-                }
-                if(backHeight == 0) {
-                    backIndex = i;
-                    backHeight = nowHeight;
-                }
-                if (nowHeight > frontHeight) {
-                    frontHeight = nowHeight;
-                    frontIndex = i;
-                    if (frontHeight == backHeight) {
-
-                    }
-                }
-            }
-        }
-    }
-}
+//package Microsoft.chapter_a;
+//
+//public class Test_M_A_10 {
+//
+//}
+//class Solution_M_A_10 {
+//    //f
+//    public int trap(int[] height) {
+//        int backIndex;
+//        int backHeight = 0;
+//        int frontIndex;
+//        int frontHeight = 0;
+//        int brick;
+//        int nowWater = 0;
+//        for (int i = 0; i < height.length; i++){
+//            int nowHeight = height[i];
+//            if (nowHeight > 0) {
+//                if(frontHeight == 0) {
+//                    frontIndex = i;
+//                    frontHeight = nowHeight;
+//                }
+//                if(backHeight == 0) {
+//                    backIndex = i;
+//                    backHeight = nowHeight;
+//                }
+//                if (nowHeight > frontHeight) {
+//                    frontHeight = nowHeight;
+//                    frontIndex = i;
+//                    if (frontHeight == backHeight) {
+//
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
--- 03_Algorithm/Microsoft/chapter_a/Test_M_A_11.java
+++ 03_Algorithm/Microsoft/chapter_a/Test_M_A_11.java
@@ -1,10 +1,13 @@
 package Microsoft.chapter_a;
 
 public class Test_M_A_11 {
-
+    public static void main(String[] args) {
+        int[][] test = new int[][]{{1, 2}, {1, 2}, {1, 2}};
+    }
 }
+
 class Solution_M_A_11 {
     public void setZeroes(int[][] matrix) {
-
+        boolean[][] result = new boolean[matrix.length][matrix.length];
     }
 }
