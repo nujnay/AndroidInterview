26e22d714f57d114328d86ec50c8d8c405e8fb56
yanjun <1292015395@qq.com>
2021-05-05 17:55:57 +0800

    master

--- 03_Algorithm/Microsoft/chapter_a/Test_M_A_11.java
+++ 03_Algorithm/Microsoft/chapter_a/Test_M_A_11.java
@@ -8,6 +8,25 @@ public class Test_M_A_11 {
 
 class Solution_M_A_11 {
     public void setZeroes(int[][] matrix) {
-        boolean[][] result = new boolean[matrix.length][matrix.length];
+        int[] resultRow = new int[matrix.length];
+        int[] resultColumn = new int[matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    resultRow[i] = 2;
+                    resultColumn[j] = 2;
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (resultRow[i] == 2) {
+                    matrix[i][j] = 0;
+                }
+                if (resultColumn[j] == 2) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
     }
 }
