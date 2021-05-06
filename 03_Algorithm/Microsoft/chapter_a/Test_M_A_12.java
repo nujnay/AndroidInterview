5ca7e2b1e405ab0f14984099a7443f08fb4994d2
yanjun <1292015395@qq.com>
2021-05-06 09:26:43 +0800

    master

---
+++ 03_Algorithm/Microsoft/chapter_a/Test_M_A_12.java
@@ -0,0 +1,19 @@
+package Microsoft.chapter_a;
+
+public class Test_M_A_12 {
+    public static void main(String[] args) {
+        int[][] test = new int[][]{{1, 2}, {1, 2}, {1, 2}};
+    }
+}
+
+class Solution_M_A_12 {
+    // 0,0 0,2 2,2 2,0
+    // 0,1 1,2 2,1 1,0
+    public void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix[0].length / 2; i++) {
+            for (int j = i; j < matrix[0].length - 1; j++) {
+
+            }
+        }
+    }
+}
