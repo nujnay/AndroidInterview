40243585461faff76a25eab058ccd93bed854308
yanjun <1292015395@qq.com>
2021-05-06 10:12:04 +0800

    master

--- 03_Algorithm/Microsoft/chapter_a/Test_M_A_12.java
+++ 03_Algorithm/Microsoft/chapter_a/Test_M_A_12.java
@@ -9,10 +9,21 @@ public class Test_M_A_12 {
 class Solution_M_A_12 {
     // 0,0 0,2 2,2 2,0
     // 0,1 1,2 2,1 1,0
+
+    //4 * 4
+    // 0,1 1,2 3,2 2,0
+    // 1,1 1,2 2,2 2,1
     public void rotate(int[][] matrix) {
         for (int i = 0; i < matrix[0].length / 2; i++) {
-            for (int j = i; j < matrix[0].length - 1; j++) {
-
+            for (int j = i; j < matrix[0].length - 1 - i; j++) {
+                int one = matrix[i][j];
+                int two = matrix[j][matrix[0].length - 1 - i];
+                int three = matrix[matrix[0].length - 1 - i][matrix[0].length - 1 - j];
+                int four = matrix[matrix[0].length - 1 - j][i];
+                matrix[i][j] = four;
+                matrix[j][matrix[0].length - 1 - i] = one;
+                matrix[matrix[0].length - 1 - i][matrix[0].length - 1 - j] = two;
+                matrix[matrix[0].length - 1 - j][i] = three;
             }
         }
     }
