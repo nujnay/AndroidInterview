e62edcc90b12fe16734ac728b30efbe68d89a805
yanjun <1292015395@qq.com>
2021-05-08 09:36:55 +0800

    master

--- 03_Algorithm/Microsoft/chapter_b/Test_M_b_06.java
+++ 03_Algorithm/Microsoft/chapter_b/Test_M_b_06.java
@@ -5,23 +5,24 @@ public class Test_M_b_06 {
 
     }
 }
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         ListNode now = null;
         for (int i = 0; i < lists.length; i++) {
-
+            now = mergeTwoLists(now, lists[i]);
         }
-
+        return now;
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
