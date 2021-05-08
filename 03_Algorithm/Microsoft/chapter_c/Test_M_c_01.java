2ab1df63a0515529922ee43a75b093f41520641f
yanjun <1292015395@qq.com>
2021-05-08 14:49:13 +0800

    master

--- 03_Algorithm/Microsoft/chapter_c/Test_M_c_01.java
+++ 03_Algorithm/Microsoft/chapter_c/Test_M_c_01.java
@@ -3,7 +3,15 @@ package Microsoft.chapter_c;
 
 public class Test_M_c_01 {
     public static void main(String[] args) {
-
+        TreeNode treeNode5 = new TreeNode();
+        TreeNode treeNode4 = new TreeNode();
+        TreeNode treeNode6 = new TreeNode();
+        TreeNode treeNode7 = new TreeNode();
+        treeNode6.left = treeNode4;
+        treeNode6.right = treeNode7;
+        treeNode5.right = treeNode6;
+        Solution_M_c_01 solution_m_c_01 = new Solution_M_c_01();
+        System.out.println(solution_m_c_01.isValidBST(treeNode5));
     }
 }
 class Solution_M_c_01 {
