c80b5825b0a7221aad8d24e5a49b2462ff84c778
yanjun <1292015395@qq.com>
2021-05-08 09:17:15 +0800

    master

--- 03_Algorithm/Microsoft/chapter_b/Test_M_b_06.java
+++ 03_Algorithm/Microsoft/chapter_b/Test_M_b_06.java
@@ -27,16 +27,19 @@ class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode result = new ListNode(0);
         ListNode now = result;
-        while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                now.next = l1;
-                l1 = l1.next;
+        ListNode a = l1;
+        ListNode b = l2;
+        while (a != null && b != null) {
+            if (a.val <= b.val) {
+                now.next = a;
+                a = a.next;
             } else {
-                now.next = l2;
-                l2 = l2.next;
+                now.next = b;
+                b = b.next;
             }
+            now = now.next;
         }
-        now.next = l1 == null ? l2 : l1;
+        now.next = a == null ? b : a;
         return result.next;
     }
 }
