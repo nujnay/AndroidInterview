b03ca6aa7002c381333621d87bd3c68c949115ed
yanjun <1292015395@qq.com>
2021-05-08 14:47:07 +0800

    master

--- 03_Algorithm/Microsoft/chapter_c/Test_M_c_01.java
+++ 03_Algorithm/Microsoft/chapter_c/Test_M_c_01.java
@@ -8,6 +8,16 @@ public class Test_M_c_01 {
 }
 class Solution_M_c_01 {
     public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
 
+    public boolean isValidBST(TreeNode node, long lower, long upper) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= lower || node.val >= upper) {
+            return false;
+        }
+        return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
     }
 }
