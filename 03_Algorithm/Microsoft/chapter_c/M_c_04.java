7708824d6a91f5b02b2b9b1c329360c59a36e0b9
yanjun <1292015395@qq.com>
2021-05-10 11:32:28 +0800

    master

---
+++ 03_Algorithm/Microsoft/chapter_c/M_c_04.java
@@ -0,0 +1,59 @@
+package Microsoft.chapter_c;
+
+import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class M_c_04 {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class Solution_M_c_04_02 {
+    List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+        return result;
+    }
+
+    class Node {
+        TreeNode now;
+        Node next;
+        int level;
+    }
+}
+
+class Solution_M_c_04 {
+    List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        addToList(root, 0);
+        for (List<Integer> list : result) {
+            Collections.sort(list);
+        }
+        return result;
+    }
+
+    public void addToList(TreeNode now, int level) {
+        if (result.size() > level) {
+            result.get(level).add(now.val);
+        } else {
+            List<Integer> list2 = new ArrayList<>();
+            list2.add(now.val);
+            result.add(list2);
+        }
+        if (now.right != null) {
+            int next = level + 1;
+            addToList(now.right, next);
+        }
+        if (now.left != null) {
+            int next = level + 1;
+            addToList(now.left, next);
+        }
+    }
+}
