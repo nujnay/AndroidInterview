8b847f7d988e4b3848f1517c54df6c00542d75ec
yanjun <1292015395@qq.com>
2021-05-10 14:35:21 +0800

    master

--- 03_Algorithm/Microsoft/chapter_d/M_d_01.java
+++ 03_Algorithm/Microsoft/chapter_d/M_d_01.java
@@ -14,27 +14,30 @@ public class M_d_01 {
 class Solution_M_d_01 {
     public List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
-        Map<String, String> map = new HashMap<>();
-        map.put("2", "abc");
-        map.put("3", "def");
-        map.put("4", "ghi");
-        map.put("5", "jkl");
-        map.put("6", "mno");
-        map.put("7", "pqrs");
-        map.put("8", "tuv");
-        map.put("9", "wxyz");
+        Map<Character, String> map = new HashMap<>();
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
         char[] digis = digits.toCharArray();
-        letterCombinations(digis, 0, map, "", result);
+        letterCombinations(0, digis, map, result, "");
         return result;
     }
 
-    public void letterCombinations(char[] digis, int index, Map<String, String> map, String part, List<String> result) {
-        int digi = digis[index];
-        String now = map.get(digi);
-
-
-        if (index == digis.length - 1) {
-            result.add(part);
+    public void letterCombinations(int index, char[] digis, Map<Character, String> map,List<String> result,String lastStr) {
+        if (index == digis.length) {
+            result.add(lastStr);
+            return;
+        }
+        char now = digis[index];
+        String digiRepresent = map.get(now);
+        for (char digiRepresentChar : digiRepresent.toCharArray()) {
+            String nowStr = lastStr + digiRepresentChar;
+            letterCombinations(index + 1, digis, map, result, nowStr);
         }
     }
 }
