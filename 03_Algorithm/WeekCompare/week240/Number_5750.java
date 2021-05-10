db6858caea9bd2eeeb9130e646cb8e35d15c3b6f
yanjun <1292015395@qq.com>
2021-05-10 09:37:45 +0800

    master

--- 03_Algorithm/WeekCompare/week240/Number_5750.java
+++ 03_Algorithm/WeekCompare/week240/Number_5750.java
@@ -1,13 +1,13 @@
-package WeekCompare.week240;
-
-public class Number_5750 {
-    public static void main(String[] args) {
-        int[][] ints = new int[][];
-    }
-}
-
-class Solution_Number_5750 {
-    public int maximumPopulation(int[][] logs) {
-
-    }
-}
+//package WeekCompare.week240;
+//
+//public class Number_5750 {
+//    public static void main(String[] args) {
+//        int[][] ints = new int[][];
+//    }
+//}
+//
+//class Solution_Number_5750 {
+//    public int maximumPopulation(int[][] logs) {
+//
+//    }
+//}
