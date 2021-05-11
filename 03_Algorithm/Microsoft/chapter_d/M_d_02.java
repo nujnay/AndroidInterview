eaf79da405c9664c143341e1721b524d0610788a
yanjun <1292015395@qq.com>
2021-05-11 15:51:20 +0800

    master

--- 03_Algorithm/Microsoft/chapter_d/M_d_02.java
+++ 03_Algorithm/Microsoft/chapter_d/M_d_02.java
@@ -59,6 +59,42 @@ class Solution_M_d_02 {
         }
     }
 
+    public void checkAllOrientation(char[][] board, char[] target, int index, int indexRow, int indexColumn, List<String> result, int orient) {
+        if (indexRow < board.length && indexRow >= 0) {
+            if (indexColumn < board[0].length && indexColumn >= 0) {
+                if (index < target.length) {
+                    char now = board[indexRow][indexColumn];
+                    if (now != '@') {
+                        if (target[index] == now) {
+                            if (index == target.length - 1) {
+                                if (!result.contains(new String(target))) {
+                                    result.add(new String(target));
+                                }
+                                return;
+                            }
+                            board[indexRow][indexColumn] = '@';
+                            char[][] boardNow = new char[board.length][board[0].length];
+                            // up:1 down:2 left:3 right:4
+                            if (orient != 1) { //up:1
+                                checkAllOrientation(boardNow, target, index + 1, indexRow + 1, indexColumn, result, 2);//down
+                            }
+                            if (orient != 2) { //down:2
+                                checkAllOrientation(boardNow, target, index + 1, indexRow - 1, indexColumn, result, 1);//up
+                            }
+                            if (orient != 3) {//left:3
+                                checkAllOrientation(boardNow, target, index + 1, indexRow, indexColumn + 1, result, 4);//right
+                            }
+                            if (orient != 4) {//right:4
+                                checkAllOrientation(boardNow, target, index + 1, indexRow, indexColumn - 1, result, 3);//left
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     public void checkAllHorizontalMovement(char[][] board, char[] target, int index, int indexRow,
                                            int indexColumn, List<String> result) {
         if (indexRow < board.length) {
