ac3bebdda696fe909069d8721ce58423b6031980
yanjun <1292015395@qq.com>
2021-05-12 10:53:12 +0800

    master

--- 03_Algorithm/Microsoft/chapter_d/M_d_03.java
+++ 03_Algorithm/Microsoft/chapter_d/M_d_03.java
@@ -44,7 +44,8 @@ class Solution_M_d_03 {
         char originalNow = original[originalIndex];
         char patternNow = pattern[patternIndex];
         if (patternNow == '*') {
-            return isMatch(original, pattern, originalIndex + 1, patternIndex);
+            return isMatch(original, pattern, originalIndex + 1, patternIndex)
+                    || isMatch(original, pattern, originalIndex + 1, patternIndex + 1);
         }
         if (patternNow == '?') {
             return isMatch(original, pattern, originalIndex + 1, patternIndex + 1);
