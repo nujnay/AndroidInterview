c72a8b887ec13010ec022f755bddc6cde102ec2f
yanjun <1292015395@qq.com>
2021-05-12 10:27:26 +0800

    master

--- 03_Algorithm/Microsoft/chapter_d/M_d_03.java
+++ 03_Algorithm/Microsoft/chapter_d/M_d_03.java
@@ -4,20 +4,38 @@ import java.lang.module.FindException;
 
 public class M_d_03 {
 }
+
 //10:11
 class Solution_M_d_03 {
     public boolean isMatch(String s, String p) {
         return isMatch(s.toCharArray(), p.toCharArray(), 0);
     }
 
-    public boolean isMatch(char[] original, char[] pattern, int originalIndex) {
+    public boolean isMatch(char[] original, char[] pattern, int originalIndex, int patternIndex) {
         char originalNow = original[originalIndex];
-        if (isMatchPatternOne()) { //需要知道现在是第几个
+        char patternNow = original[patternIndex];
+        if (originalIndex == original.length - 1) {
+            if (patternIndex == pattern.length - 1) {
+                if (originalNow == patternNow) {
+                    return true;
+                }
+                if (patternNow == '*') {
+                    return true;
+                }
+                if (patternNow == '?') {
+                    return true;
+                }
+                return false;
+            }
+        } else if (originalIndex >= original.length) {
+            return false;
+        } else if (patternIndex >= pattern.length) {
+            return false;
+        }
 
+        if (patternNow == '*') {
+            isMatch(original, pattern, originalIndex + 1, patternIndex);
         }
     }
 
-    public boolean isMatchPatternOne() {
-
-    }
 }
