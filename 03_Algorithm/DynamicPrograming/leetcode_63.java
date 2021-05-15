5bb00f492fbb3ebfdfc3c23ec345b7528a24a138
yanjun <1292015395@qq.com>
2021-05-15 17:55:58 +0800

    master

--- 03_Algorithm/DynamicPrograming/leetcode_63.java
+++ 03_Algorithm/DynamicPrograming/leetcode_63.java
@@ -1,7 +1,7 @@
 package DynamicPrograming;
 
 public class leetcode_63 {
-    int[] nums = new int[]{2, 3, 1, 4, -1, -2};
+    int[] nums = new int[]{1, -1, 2, 3, -4};
     //遇到负数才会截断
 
 }
@@ -10,7 +10,8 @@ class Solution {
     public int maxSubArray(int[] nums) {
         int pre = 0, maxAns = nums[0];
         for (int x : nums) {
-            pre = Math.max(pre + x, x);//确定不为负数
+            //只要不为负数就是所有的数累加，如果此时的
+            pre = Math.max(pre + x, x);
             maxAns = Math.max(maxAns, pre);
         }
         return maxAns;
