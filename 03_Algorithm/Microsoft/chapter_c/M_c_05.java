bb65b4ace3ed5b92783e049307c53bfd01539d65
yanjun <1292015395@qq.com>
2021-05-17 14:17:50 +0800

    master

--- 03_Algorithm/Microsoft/chapter_c/M_c_05.java
+++ 03_Algorithm/Microsoft/chapter_c/M_c_05.java
@@ -1,10 +1,42 @@
 package Microsoft.chapter_c;
 
-public class M_c_05 {
+import java.util.LinkedList;
 
+public class M_c_05 {
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println(++i);
+    }
 }
+
 class Solution_M_c_05 {
     public Node connect(Node root) {
-
+        if (root == null) {
+            return null;
+        }
+        //输入node 返回node 同级别 左边的next是右边的
+        LinkedList<Node> list = new LinkedList<>();
+        list.offer(root);
+        Node result = root;
+        while (!list.isEmpty()) {
+            int size = list.size();
+            LinkedList<Node> listNext = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                Node node = list.poll();
+                if (list.isEmpty()) {
+                    node.next = null;
+                } else {
+                    node.next = list.peek();
+                }
+                if (node.left != null) {
+                    listNext.offerLast(node.left);
+                }
+                if (node.right != null) {
+                    listNext.offerLast(node.right);
+                }
+            }
+            list.addAll(listNext);
+        }
+        return result;
     }
 }
