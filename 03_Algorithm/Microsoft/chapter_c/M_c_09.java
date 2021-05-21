2720bf364072cf8fd25e0441e0361692c0c31032
yanjun <1292015395@qq.com>
2021-05-21 15:47:10 +0800

    master

--- 03_Algorithm/Microsoft/chapter_c/M_c_09.java
+++ 03_Algorithm/Microsoft/chapter_c/M_c_09.java
@@ -1,28 +1,74 @@
 package Microsoft.chapter_c;
 
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class M_c_09 {
-
+    public static void main(String[] args) {
+        Solution_M_c_09_2 m_c_09_2 = new Solution_M_c_09_2();
+        int[] pre = new int[]{1, 2, 3};
+        int[] in = new int[]{2, 1, 3};
+        m_c_09_2.buildTree(pre, in);
+    }
 }
 
 //11:32-12:00
 class Solution_M_c_09 {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        TreeNode treeNode = new TreeNode();
-        treeNode.val = preorder[0];
-        for (int i = 1; i < preorder.length; i++) {
-            getIndex(inorder, preorder[i]);
+//    public TreeNode buildTree(int[] preorder, int[] inorder) {
+//        TreeNode treeNode = new TreeNode();
+//        treeNode.val = preorder[0];
+//        for (int i = 1; i < preorder.length; i++) {
+//            getIndex(inorder, preorder[i]);
+//        }
+//    }
+//
+//    int getIndex(int[] order, int target) {
+//        for (int i = 0; i < order.length; i++) {
+//            if (order[i] == target) {
+//                return i;
+//            }
+//        }
+//        return 0;
+//    }
+}
+
+//15:10-
+class Solution_M_c_09_2 {
+    private Map<Integer, Integer> indexMap;
+
+    public TreeNode myBuildTree(int[] preorder, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
+        System.out.println(preorder_left + "|" + preorder_right + "|" + inorder_left + "|" + inorder_right);
+        if (preorder_left > preorder_right) {
+            return null;
         }
+
+        // 前序遍历中的第一个节点就是根节点
+        int preorder_root = preorder_left;
+        // 在中序遍历中定位根节点
+        int inorder_root = indexMap.get(preorder[preorder_root]);
+
+        // 先把根节点建立出来
+        TreeNode root = new TreeNode(preorder[preorder_root]);
+        // 得到左子树中的节点数目
+        int size_left_subtree = inorder_root - inorder_left;
+        // 递归地构造左子树，并连接到根节点
+        // 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
+        root.left = myBuildTree(preorder, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1);
+        // 递归地构造右子树，并连接到根节点
+        // 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
+        root.right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right);
+        return root;
     }
 
-    int getIndex(int[] order,int target) {
-        for (int i = 0; i < order.length; i++) {
-            if (order[i] == target) {
-                return i;
-            }
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        // 构造哈希映射，帮助我们快速定位根节点
+        indexMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(inorder[i], i);
         }
-        return 0;
+        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
     }
 }
