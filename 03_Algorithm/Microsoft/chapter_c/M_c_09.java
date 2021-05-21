d0f13964d2fa3f0e94ce7d8eff975d5ea7b5bd0a
yanjun <1292015395@qq.com>
2021-05-21 15:06:50 +0800

    master

---
+++ 03_Algorithm/Microsoft/chapter_c/M_c_09.java
@@ -0,0 +1,28 @@
+package Microsoft.chapter_c;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class M_c_09 {
+
+}
+
+//11:32-12:00sh
+class Solution_M_c_09 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        TreeNode treeNode = new TreeNode();
+        treeNode.val = preorder[0];
+        for (int i = 1; i < preorder.length; i++) {
+            getIndex(inorder, preorder[i]);
+        }
+    }
+
+    int getIndex(int[] order,int target) {
+        for (int i = 0; i < order.length; i++) {
+            if (order[i] == target) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
---
+++ 03_Algorithm/Microsoft/chapter_g/M_g_04.java
@@ -0,0 +1,25 @@
+package Microsoft.chapter_g;
+
+public class M_g_04 {
+}
+//class LRUCache_M_g_04 {
+//
+//    public LRUCache(int capacity) {
+//
+//    }
+//
+//    public int get(int key) {
+//
+//    }
+//
+//    public void put(int key, int value) {
+//
+//    }
+//}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
