e4e89cb7d15c54b6812f521947a42c3d184cd93f
yanjun <1292015395@qq.com>
2021-05-24 11:38:28 +0800

    master

--- 03_Algorithm/Microsoft/chapter_c/M_c_09.java
+++ 03_Algorithm/Microsoft/chapter_c/M_c_09.java
@@ -13,9 +13,8 @@ public class M_c_09 {
         m_c_09_2.buildTree(pre, in);
     }
 }
-
-//11:32-12:00 19:06-
-class Solution_M_c_09 {
+//11:38
+class Solution_M_c_09_03 {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         TreeNode treeNode = new TreeNode();
         treeNode.val = preorder[0];
@@ -23,6 +22,26 @@ class Solution_M_c_09 {
             getIndex(inorder, preorder[i]);
         }
     }
+
+    int getIndex(int[] order, int target) {
+        for (int i = 0; i < order.length; i++) {
+            if (order[i] == target) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
+
+//11:32-12:00 19:06-
+class Solution_M_c_09 {
+//    public TreeNode buildTree(int[] preorder, int[] inorder) {
+//        TreeNode treeNode = new TreeNode();
+//        treeNode.val = preorder[0];
+//        for (int i = 1; i < preorder.length; i++) {
+//            getIndex(inorder, preorder[i]);
+//        }
+//    }
 
     int getIndex(int[] order, int target) {
         for (int i = 0; i < order.length; i++) {
