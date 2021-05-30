28285b20a5337f259eee0a640b1c3add16af2ce3
yanjun <1292015395@qq.com>
2021-05-30 21:57:40 +0800

    master

--- 03_Algorithm/leetcode/l_0028.java
+++ 03_Algorithm/leetcode/l_0028.java
@@ -6,6 +6,37 @@ public class l_0028 {
     }
 }
 
+//不用递归的方式 21：46 - 21:57
+class Solution_l_0028_02 {
+    public int strStr(String haystack, String needle) {
+        char[] haystackChars = haystack.toCharArray();
+        char[] needleChars = needle.toCharArray();
+        if (haystackChars.length == 0 && needleChars.length == 0) {
+            return 0;
+        }
+        for (int i = 0; i < haystackChars.length; i++) {
+            boolean nowStatusIsRight = true;
+            for (int j = 0; j < needleChars.length; j++) {
+                //只要一个为false 全部为false
+                if (i + j == haystackChars.length) {
+                    nowStatusIsRight = false;
+                    break;
+                }
+                if (haystackChars[i + j] == needleChars[j]) {
+                    nowStatusIsRight = true;
+                } else {
+                    nowStatusIsRight = false;
+                    break;
+                }
+            }
+            if (nowStatusIsRight) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
 //21:35 21：46 用递归的方法超时
 class Solution_l_0028 {
     public int strStr(String haystack, String needle) {
