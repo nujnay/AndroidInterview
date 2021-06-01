f3b1f5d1a24162cede0ba8f3a67c7efb073c3c8d
yanjun <1292015395@qq.com>
2021-06-01 12:06:54 +0800

    master

--- 03_Algorithm/leetcode/l_0053.java
+++ 03_Algorithm/leetcode/l_0053.java
@@ -1,9 +1,23 @@
 package leetcode;
 
 public class l_0053 {
+    //[5,4,-1,7,8]
 }
+//11: 59
 class Solution_l_0053 {
     public int maxSubArray(int[] nums) {
-
+        int amountNow = 0;
+        int maxSumNow = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int oldAmount = amountNow;
+            amountNow = amountNow + nums[i];
+            if (amountNow < oldAmount) {
+                amountNow = oldAmount;
+            }
+            if (amountNow > maxSumNow) {
+                maxSumNow = amountNow;
+            }
+        }
+        return maxSumNow;
     }
 }
