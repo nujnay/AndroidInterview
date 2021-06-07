269708e2717801a5cc254ef5d6654bbccc468a37
yanjun <1292015395@qq.com>
2021-06-07 10:01:33 +0800

    master

---
+++ 03_Algorithm/leetcode/l_0541.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+public class l_0541 {
+    //abcdefg
+    //bacdfeg
+    public static void main(String[] args) {
+        Solution_l_0541 solution_l_0541 = new Solution_l_0541();
+        System.out.println(solution_l_0541.reverseStr("abcdefg", 2));
+    }
+}
+
+//09：39
+class Solution_l_0541 {
+    public String reverseStr(String s, int k) {
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            int a = i / k;
+            int c = a % 2;
+            int b = i % k;
+            System.out.println("i:" + i + ":" + "a:" + a + ":" + "b:" + b + ":" + "c:" + c);
+            if (c == 0) {
+                if (b < k / 2) {
+                    if (i + k - 1 - b < chars.length) {
+                        char tmp = chars[i];
+                        chars[i] = chars[i + k - 1 - b];
+                        chars[k - 1 - b] = tmp;
+                    }
+                }
+            }
+        }
+        return new String(chars);
+    }
+}
