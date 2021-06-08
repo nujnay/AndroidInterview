57711fbcc756391bd23f3b8940fac34531c7f825
yanjun <1292015395@qq.com>
2021-06-08 11:52:13 +0800

    master

--- 03_Algorithm/sort/sort_1/BubbleSort.java
+++ 03_Algorithm/sort/sort_1/BubbleSort.java
@@ -1,7 +1,12 @@
 package sort.sort_1;
 
 public class BubbleSort {
-    public void BubbleSort(int[] arr) {
+
+    public static void main(String[] args) {
+        BubbleSort(new int[]{1, 2, 5, 4});
+    }
+
+    public static void BubbleSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] < arr[j + 1]) {
@@ -11,7 +16,7 @@ public class BubbleSort {
         }
     }
 
-    public void swap(int[] arr, int i, int j) {
+    public static void swap(int[] arr, int i, int j) {
         int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
