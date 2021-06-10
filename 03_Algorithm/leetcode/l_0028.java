3c341070b7eb61a4af9c981a2a46eb8623485dfe
yanjun <1292015395@qq.com>
2021-06-10 10:24:35 +0800

    master

--- 03_Algorithm/leetcode/l_0028.java
+++ 03_Algorithm/leetcode/l_0028.java
@@ -108,9 +108,13 @@ class Solution_l_0028_KMP {
         // 构造过程 i = 2，j = 0 开始，i 小于等于匹配串长度 【构造 i 从 2 开始】
         for (int i = 2, j = 0; i <= m; i++) {
             // 匹配不成功的话，j = next(j)
-            while (j > 0 && p[i] != p[j + 1]) j = next[j];
+            while (j > 0 && p[i] != p[j + 1]) {
+                j = next[j];
+            }
             // 匹配成功的话，先让 j++
-            if (p[i] == p[j + 1]) j++;
+            if (p[i] == p[j + 1]){
+                j++;
+            }
             // 更新 next[i]，结束本次循环，i++
             next[i] = j;
         }
@@ -118,11 +122,17 @@ class Solution_l_0028_KMP {
         // 匹配过程，i = 1，j = 0 开始，i 小于等于原串长度 【匹配 i 从 1 开始】
         for (int i = 1, j = 0; i <= n; i++) {
             // 匹配不成功 j = next(j)
-            while (j > 0 && s[i] != p[j + 1]) j = next[j];
+            while (j > 0 && s[i] != p[j + 1]){
+                j = next[j];
+            }
             // 匹配成功的话，先让 j++，结束本次循环后 i++
-            if (s[i] == p[j + 1]) j++;
+            if (s[i] == p[j + 1]) {
+                j++;
+            }
             // 整一段匹配成功，直接返回下标
-            if (j == m) return i - m;
+            if (j == m){
+                return i - m;
+            }
         }
 
         return -1;
