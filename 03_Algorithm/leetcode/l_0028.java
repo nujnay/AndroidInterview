b837d7f7dca5d690e4384c9ede69e21d41b55e96
yanjun <1292015395@qq.com>
2021-06-10 18:02:38 +0800

    master

--- 03_Algorithm/leetcode/l_0028.java
+++ 03_Algorithm/leetcode/l_0028.java
@@ -6,21 +6,11 @@ public class l_0028 {
     public static void main(String[] args) {
         //abababcd
         //ababcd
-        //001200
-        //abababcd
-        //  ababcd
 
-        //aaaaaecd
-        //aaabcd
-        //012000
-        //aaaaaecd
-        //  aaabcd
-        //aaaaaecd
-        //    aaabcd
-        //aaaaaecd
-        //     aaabcd
+        //hello
+        //ll
         Solution_l_0028_KMP_mine solution_l_0028_kmp_mine = new Solution_l_0028_KMP_mine();
-        System.out.println(solution_l_0028_kmp_mine.strStr("aaaaaecd", "aaabcd"));
+        System.out.println(solution_l_0028_kmp_mine.strStr("hello", "ll"));
     }
 }
 
@@ -47,9 +37,12 @@ class Solution_l_0028_KMP_mine {
                 needleIndex++;
             } else {
                 //如果不一样 移动上一位的数值的下一个 如果相等继续移动 知道needleIndex = 0
-                needleIndex = next[needleIndex - 1] + 1;
-                if (haystackC[i] == needleC[needleIndex]) {
-                    needleIndex++;
+                if (needleIndex > 0) {
+                    System.out.println("needleIndex:" + needleIndex + "||" + i);
+                    needleIndex = next[needleIndex - 1] + 1;
+                    if (haystackC[i] == needleC[needleIndex]) {
+                        needleIndex++;
+                    }
                 }
             }
             if (needleIndex == needle.length() - 1) {
