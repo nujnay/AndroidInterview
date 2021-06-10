a55b4aee804c46f929f8161ae9b154ddbd298c56
yanjun <1292015395@qq.com>
2021-06-10 10:07:17 +0800

    master

---
+++ 03_Algorithm/sort/sort_1/jz_45.java
@@ -0,0 +1,16 @@
+package sort.sort_1;
+
+public class jz_45 {
+/**
+ * 输入: [10,2]
+ * 输出: "102"
+ *
+ * 输入: [3,30,34,5,9]
+ * 输出: "3033459"
+ */
+}
+class Solution {
+    public String minNumber(int[] nums) {
+
+    }
+}
