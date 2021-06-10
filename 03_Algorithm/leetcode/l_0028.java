ac669005e71e7cb527ed10d905ac44131c77943c
yanjun <1292015395@qq.com>
2021-06-10 10:22:10 +0800

    master

--- 03_Algorithm/leetcode/l_0028.java
+++ 03_Algorithm/leetcode/l_0028.java
@@ -89,3 +89,39 @@ class Solution_l_0028 {
         }
     }
 }
+
+class Solution_l_0028_KMP {
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) {
+            return 0;
+        }
+        int n = haystack.length(), m = needle.length();
+        haystack = " " + haystack;
+        needle = " " + needle;
+        char[] s = haystack.toCharArray();
+        char[] p = haystack.toCharArray();
+        int[] next = new int[m + 1];
+        for (int i = 2, j = 0; i <= m; i++) {
+            while (j > 0 && p[i] != p[j + 1]) {
+                j = next[j];
+            }
+            if (p[i] == p[j + 1]) {
+                j++;
+            }
+            next[i] = j;
+        }
+        for (int i = 1, j = 0; i <= n; i++) {
+            while (j > 0 && s[i] != p[j + 1]) {
+                j = next[j];
+            }
+            if (s[i] == p[j + 1]) {
+                j++;
+            }
+            if (j == m) {
+                return i - m;
+            }
+        }
+        return -1;
+    }
+
+}
