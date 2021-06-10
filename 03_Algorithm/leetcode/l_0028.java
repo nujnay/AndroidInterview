8414af774031acf1610382cd8d1af68fce97098a
yanjun <1292015395@qq.com>
2021-06-10 10:09:16 +0800

    master

--- 03_Algorithm/leetcode/l_0028.java
+++ 03_Algorithm/leetcode/l_0028.java
@@ -6,6 +6,28 @@ public class l_0028 {
     }
 }
 
+/**
+ * mplement strStr().
+ *
+ * Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ *
+ * Clarification:
+ *
+ * What should we return when needle is an empty string? This is a great question to ask during an interview.
+ *
+ * For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+ *
+ *  
+ *
+ *
+ * Input: haystack = "hello", needle = "ll"
+ * Output: 2
+ * Input: haystack = "aaaaa", needle = "bba"
+ * Output: -1
+ * Input: haystack = "", needle = ""
+ * Output: 0
+ */
+
 //不用递归的方式 21：46 - 21:57
 class Solution_l_0028_02 {
     public int strStr(String haystack, String needle) {
