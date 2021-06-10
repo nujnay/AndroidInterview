47720edf4a8121eb73bb10d32aec90588e12bb03
yanjun <1292015395@qq.com>
2021-06-10 10:54:37 +0800

    master

--- 03_Algorithm/leetcode/l_0028.java
+++ 03_Algorithm/leetcode/l_0028.java
@@ -1,9 +1,11 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class l_0028 {
     public static void main(String[] args) {
         Solution_l_0028_KMP_2 solution_l_0028_kmp_2 = new Solution_l_0028_KMP_2();
-        solution_l_0028_kmp_2.strStr("abc", "abc");
+        solution_l_0028_kmp_2.strStr("abc", "aaabaaa");
     }
 }
 
@@ -156,7 +158,7 @@ class Solution_l_0028_KMP_2 {
             }
             pi[i] = j;
         }
-        System.out.println(pi);
+        System.out.println(Arrays.toString(pi));
         for (int i = 0, j = 0; i < n; i++) {
             while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
                 j = pi[j - 1];
