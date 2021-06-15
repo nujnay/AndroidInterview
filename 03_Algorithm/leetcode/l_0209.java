7afd1d81a1addde0538e513155f0fee017f6aece
yanjun <1292015395@qq.com>
2021-06-15 11:15:58 +0800

    master

--- 03_Algorithm/leetcode/l_0209.java
+++ 03_Algorithm/leetcode/l_0209.java
@@ -15,16 +15,16 @@ class Solution_l_0209 {
         if (nums.length == 0) {
             return 0;
         }
-        int sum = 0;
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < nums.length; i++) {
-            sum = 0;
+            int sum = 0;
             for (int j = i; j < nums.length; j++) {
                 sum = sum + nums[j];
-                if (sum == target) {
+                if (sum >= target) {
                     if (j - i + 1 < min) {
                         min = j - i + 1;
                     }
+                    break;
                 }
             }
         }
