7c4239bbb09a524fbe26e9bc2b900efef4a9ffd7
yanjun <1292015395@qq.com>
2021-06-15 10:45:26 +0800

    master

--- 03_Algorithm/leetcode/l_0209.java
+++ 03_Algorithm/leetcode/l_0209.java
@@ -1,9 +1,15 @@
 package leetcode;
 
 public class l_0209 {
+    public static void main(String[] args) {
+        //11
+        //[1,2,3,4,5]
+        Solution_l_0209 solution_l_0209 = new Solution_l_0209();
+        System.out.println(solution_l_0209.minSubArrayLen(11, new int[]{1, 2, 3, 4, 5}));
+    }
 }
 
-//09:41
+//09:41-10:09
 class Solution_l_0209 {
     public int minSubArrayLen(int target, int[] nums) {
         if (nums.length == 0) {
@@ -12,6 +18,7 @@ class Solution_l_0209 {
         int sum = 0;
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < nums.length; i++) {
+            sum = 0;
             for (int j = i; j < nums.length; j++) {
                 sum = sum + nums[j];
                 if (sum == target) {
