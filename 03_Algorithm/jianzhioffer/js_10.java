1cc958bce770030b30ab107dc9677890a600a49e
yanjun <1292015395@qq.com>
2021-07-13 22:03:25 +0800

    master

--- 03_Algorithm/jianzhioffer/js_10.java
+++ 03_Algorithm/jianzhioffer/js_10.java
@@ -9,7 +9,9 @@ package jianzhioffer;
 public class js_10 {
     public static void main(String[] args) {
         Solution_js_10 solution_js_10 = new Solution_js_10();
-        System.out.println(solution_js_10.fib(5));
+        Solution_js_10_2 Solution_js_10_2 = new Solution_js_10_2();
+        System.out.println(solution_js_10.fib(50));
+        System.out.println(Solution_js_10_2.fib(50));
 
     }
 }
@@ -31,8 +33,20 @@ class Solution_js_10 {
 
     public int fib1(int now, int n, int last, int lastlast) {
         if (now == n) {
-            return last + lastlast;
+            return (last % 1000000007 + lastlast % 1000000007) % 1000000007;
         }
-        return fib1(now + 1, n, last + lastlast, last);
+        return fib1(now + 1, n, (last + lastlast) % 1000000007, last % 1000000007);
+    }
+}
+
+class Solution_js_10_2 {
+    public int fib(int n) {
+        int a = 0, b = 1, sum;
+        for (int i = 0; i < n; i++) {
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return a;
     }
 }
