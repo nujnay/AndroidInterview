c69e95dd1ec9f323768b1e0eab4383d12a057ae1
yanjun <1292015395@qq.com>
2021-07-16 14:31:17 +0800

    master

---
+++ 03_Algorithm/Microsoft/l_0405.java
@@ -0,0 +1,32 @@
+package Microsoft;
+
+import Microsoft.chapter_c.TreeNode;
+
+/**
+ * @program: Inter
+ * @description:
+ * @author: YanJun
+ * @create: 2021-07-14 09:39
+ **/
+public class l_0405 {
+}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution_l_0405 {
+    public TreeNode deleteNode(TreeNode root, int key) {
+
+    }
+}
