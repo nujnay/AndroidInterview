69e2fa95435ddc8d6a16eede4d90669dcc3be8b5
yanjun <1292015395@qq.com>
2021-08-07 11:58:34 +0800

    master

--- 03_Algorithm/bitdance/BitDance_08_07_0003.java
+++ 03_Algorithm/bitdance/BitDance_08_07_0003.java
@@ -5,15 +5,29 @@ package bitdance;
  * @description:
  * @author: YanJun
  * @create: 2021-08-07 11:36
+ * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
  **/
 public class BitDance_08_07_0003 {
 
 }
-//abcabcbb
+
 class Solution_BitDance_08_07_0003 {
     public int lengthOfLongestSubstring(String s) {
+        char[] original = s.toCharArray();
+        if (original.length == 0) {
+            return 0;
+        }
+        Node node = new Node();
+        node.val = original[0];
+
+        for (int i = 0; i < original.length; i++) {
+
+        }
+
 
         return 0;
-
     }
+
+
+
 }
---
+++ 03_Algorithm/bitdance/Node.java
@@ -0,0 +1,12 @@
+package bitdance;
+
+/**
+ * @program: Inter
+ * @description:
+ * @author: YanJun
+ * @create: 2021-08-07 11:41
+ **/
+public class Node {
+    char val;
+    Node next;
+}
