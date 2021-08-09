a9758a39c40ce87b7cba8969a5d41ea896e2036b
yanjun <1292015395@qq.com>
2021-08-09 21:21:03 +0800

    master

--- 03_Algorithm/bitdance/BitDance_08_07_0003.java
+++ 03_Algorithm/bitdance/BitDance_08_07_0003.java
@@ -14,15 +14,6 @@ public class BitDance_08_07_0003 {
 class Solution_BitDance_08_07_0003 {
     public int lengthOfLongestSubstring(String s) {
         char[] original = s.toCharArray();
-        if (original.length == 0) {
-            return 0;
-        }
-        Node node = new Node();
-        node.val = original[0];
-
-        for (int i = 0; i < original.length; i++) {
-
-        }
 
 
         return 0;
