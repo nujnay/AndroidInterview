7a592e799a756bb257c6bd21ced2ccf812f5d1d6
yanjun <1292015395@qq.com>
2021-08-10 10:17:50 +0800

    master

--- 03_Algorithm/bitdance/BitDance_08_07_0003.java
+++ 03_Algorithm/bitdance/BitDance_08_07_0003.java
@@ -1,5 +1,8 @@
 package bitdance;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @program: Inter
  * @description:
@@ -14,11 +17,22 @@ public class BitDance_08_07_0003 {
 class Solution_BitDance_08_07_0003 {
     public int lengthOfLongestSubstring(String s) {
         char[] original = s.toCharArray();
-
-
-        return 0;
+        Set<Character> set = new HashSet<>();
+        int n = original.length;
+        int rk = -1;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (i != 0) {
+                set.remove(original[i - 1]);
+            }
+            while (rk + 1 < n && !set.contains(original[rk + 1])) {
+                set.add(original[rk + 1]);
+                rk++;
+            }
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
     }
-
 
 
 }
