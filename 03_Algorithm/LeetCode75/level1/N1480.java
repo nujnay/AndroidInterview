af7bf26168255c5f0f9b291c654a1cde4ed1d04c
Your Name <you@example.com>
2022-10-31 15:53:42 +0800

    master

--- 03_Algorithm/LeetCode75/level1/N1480.java
+++ 03_Algorithm/LeetCode75/level1/N1480.java
@@ -1,5 +1,6 @@
 package LeetCode75.level1;
 /*
+Running Sum of 1d Array
 Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 Return the running sum of nums.
 Example 1:
